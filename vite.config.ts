ec2ee2d363c0f301daf620f3f555610f1d0be812
Simon De Wilde <simon.dewilde2000@gmail.com>
2024-02-03 11:39:32 +0100

    support importing .svg files as React components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({
+    // Make all imported .svg files useable as React components
+    include: "**/*.svg",
+  })],
 })
