854b47a0d5f48d438b7b2b4df04779901f9cf376
Simon De Wilde <simon.dewilde2000@gmail.com>
2024-02-03 11:40:12 +0100

    style header and add cookie icon

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,21 @@
-import { Typography } from '@mui/joy';
+import { Box, Stack, Typography } from '@mui/joy';
+import CookieIcon from '../../assets/images/cookie-icon.svg';
 
 const Header = () => {
   return (
-    <>
-      <Typography level="h1">Recepten Tramstraat</Typography>
-      <Typography level="body-lg">
-        Recepten uit het groene schrift en meer!
-      </Typography>
-    </>
+    <Box sx={{p: 2}}>
+      <Stack direction={'column'} alignItems={'center'}>
+        <Stack direction={'row'} justifyContent={'center'} gap={1}>
+          <Typography level="h1">Recepten Tramstraat</Typography> 
+            <Box sx={{ height: '3rem'}}>
+              <CookieIcon />
+            </Box>
+        </Stack>
+        <Typography level="body-md">
+          Recepten uit het groene schrift en meer!
+        </Typography>
+      </Stack>
+    </Box>
   );
 };
 
