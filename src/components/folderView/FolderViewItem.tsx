323da770359a719b5b2510706d14abaf4860e0aa
Simon De Wilde <simon.dewilde2000@gmail.com>
2024-02-04 14:22:48 +0100

    add icons on the treeItems

--- src/components/folderView/FolderViewItem.tsx
+++ src/components/folderView/FolderViewItem.tsx
@@ -1,6 +1,7 @@
 import { TreeItem, useTreeItem } from '@mui/x-tree-view';
 import { FolderNode } from './models';
 import { useEffect } from 'react';
+import { FolderOpen, Source, TextSnippetOutlined } from '@mui/icons-material';
 
 type FolderViewItemProps = {
 	node: FolderNode;
@@ -9,15 +10,32 @@ type FolderViewItemProps = {
 const FolderViewItem: React.FC<FolderViewItemProps> = ({ node }) => {
 	const { selected } = useTreeItem(node.path);
 
+	const isFolder = node.children;
+
 	useEffect(() => {
 		if (selected) {
 			console.log(node);
 		}
 	}, [selected, node]);
 
+	const sortedChildren = node.children?.sort((a, b) => {
+		return a.children && !b.children
+			? -1
+			: !a.children && b.children
+				? 1
+				: a.name.localeCompare(b.name);
+	});
+
 	return (
-		<TreeItem key={node.path} nodeId={node.path} label={node.name}>
-			{node.children?.map((child) => (
+		<TreeItem
+			key={node.path}
+			nodeId={node.path}
+			label={node.name}
+			expandIcon={<Source />}
+			collapseIcon={<FolderOpen />}
+			icon={!isFolder && <TextSnippetOutlined />}
+		>
+			{sortedChildren?.map((child) => (
 				<FolderViewItem key={child.path} node={child} />
 			))}
 		</TreeItem>
