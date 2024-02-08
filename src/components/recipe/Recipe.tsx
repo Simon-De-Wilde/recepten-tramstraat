c5a61134d6e6275e9e5cccc8612b76e0c50671d7
Simon De Wilde <simon.dewilde2000@gmail.com>
2024-02-08 21:01:02 +0100

    render the md files

--- src/components/recipe/Recipe.tsx
+++ src/components/recipe/Recipe.tsx
@@ -1,22 +1,35 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Box } from '@mui/joy';
 import { useFolderStructure } from 'hooks/useFolderStructure';
 import { useParams } from 'react-router';
 import { searchFolderNode } from './models';
+import Markdown from 'react-markdown';
 
 const Recipe: React.FC = () => {
 	const folderStructure = useFolderStructure();
 
 	const { name } = useParams();
 
-	const file = useMemo(
+	const [recipeText, setRecipeText] = useState('');
+
+	const node = useMemo(
 		() => searchFolderNode(folderStructure, name ?? ''),
 		[folderStructure, name],
 	);
 
-	if (file === null) return <div>404: NOT FOUND</div>; // TODO: 404 page
+	useEffect(() => {
+		fetch(node?.path ?? '')
+			.then((response) => response.text())
+			.then((text) => setRecipeText(text));
+	}, [node?.path]);
 
-	return <Box>{JSON.stringify(file)}</Box>;
+	if (node === null) return <div>404: NOT FOUND</div>; // TODO: 404 page
+
+	return (
+		<Box>
+			<Markdown>{recipeText}</Markdown>
+		</Box>
+	);
 };
 
 export default Recipe;
