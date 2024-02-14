26a12af579ae199677e34e5b989086c4922c4aca
Simon De Wilde <simon.dewilde2000@gmail.com>
2024-02-14 22:05:02 +0100

    Add text search

--- src/shared/FolderNode.models.ts
+++ src/shared/FolderNode.models.ts
@@ -2,6 +2,7 @@ export class FolderNode {
 	name: string;
 	path: string;
 	children: FolderNode[];
+	// TODO: store if the node is a folder so the folderview can hide a folder without children
 
 	constructor(name: string, path: string) {
 		this.name = name;
@@ -11,7 +12,6 @@ export class FolderNode {
 }
 
 export const isFolder = (node: FolderNode) => !!node.children.length;
-
 
 export const searchFolderNode = (
 	node: FolderNode,
@@ -27,4 +27,42 @@ export const searchFolderNode = (
 	}
 
 	return result;
+};
+
+/**
+ * Filters a node by a value
+ * @param node node to be filtered
+ * @param filterValue value which will decide if the node is filtered out or not
+ */
+export const filterFolderNode = (
+	node: FolderNode,
+	filterValue: string,
+): FolderNode => {
+	node = { ...node }; // Clone to avoid issues with overriding base values
+
+	const isHit = (n: FolderNode) =>
+		n.name.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase());
+
+	const nodeIsFolder = isFolder(node);
+
+	if (nodeIsFolder) {
+		const folderIsHit = isHit(node);
+
+		if (!folderIsHit) {
+			// Folder should be kept, but children should be filtered
+			const filteredChildren = node.children
+				.filter((child) =>
+					isFolder(child)
+						? true // Keep folder for further filtering
+						: isHit(child),
+				)
+				.map((child) =>
+					folderIsHit ? child : filterFolderNode(child, filterValue),
+				);
+
+			node.children = filteredChildren;
+		}
+	}
+
+	return node;
 };
