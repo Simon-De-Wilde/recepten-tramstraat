625430e246e8de035454166865229fc3cfba419a
Simon De Wilde <simon.dewilde2000@gmail.com>
2024-02-15 22:02:16 +0100

    put the Cookie icon above the title on mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,32 @@
 import { Grid, Typography } from '@mui/joy';
 import CookieIcon from 'assets/images/cookie-icon.svg';
+import useIsMobile from 'hooks/useIsMobile';
 
 const Header = () => {
+	const isMobile = useIsMobile();
+
 	return (
 		<Grid
 			container
 			sx={{ p: 2 }}
 			justifyContent={'center'}
-			gap={2}
+			gap={1}
 			textAlign={'center'}
 		>
-			<Grid>
-				<Typography level="h1">Recepten Tramstraat</Typography>
-			</Grid>
-			<Grid sx={{ height: '3rem' }}>
-				<CookieIcon />
+			<Grid xs={12}>
+				<Grid
+					container
+					gap={2}
+					justifyContent={'center'}
+					direction={isMobile ? 'column-reverse' : 'row'}
+				>
+					<Grid>
+						<Typography level="h1">Recepten Tramstraat</Typography>
+					</Grid>
+					<Grid sx={{ height: '3rem' }}>
+						<CookieIcon />
+					</Grid>
+				</Grid>
 			</Grid>
 			<Grid xs={12}>
 				<Typography level="body-md">
