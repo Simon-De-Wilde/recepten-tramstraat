7b372cb9387d0faccbe80bce5e6418e2bcda0f62
Simon De Wilde <simon.dewilde2000@gmail.com>
2024-02-15 21:42:52 +0100

    collapse the treeview by default if mobile

--- src/components/folderView/FolderView.tsx
+++ src/components/folderView/FolderView.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import { TreeView } from '@mui/x-tree-view';
-import { Box, Stack } from '@mui/joy';
+import { Box, Stack, useTheme } from '@mui/joy';
 import FolderViewItem from './folderViewItem/FolderViewItem';
 import { useFolderStructure } from 'hooks/useFolderStructure';
 import TreeTextSearch from 'components/folderView/textSearch/TextSearch';
+import { useMediaQuery } from '@mui/material';
 
 const FolderView: React.FC = () => {
 	const folderStructure = useFolderStructure();
 
 	const [filteredFolderStructure, setFilteredFolderStructure] =
 		useState(folderStructure);
+
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
 	return (
 		<Box
@@ -23,7 +27,9 @@ const FolderView: React.FC = () => {
 				<TreeTextSearch setFilteredroot={setFilteredFolderStructure} />
 				<TreeView
 					sx={{ p: 1 }}
-					defaultExpanded={[filteredFolderStructure.path]}
+					defaultExpanded={
+						isMobile ? undefined : [filteredFolderStructure.path]
+					}
 				>
 					<FolderViewItem node={filteredFolderStructure} />
 				</TreeView>
