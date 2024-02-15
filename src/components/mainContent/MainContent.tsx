7d06535f1d052679408d9377c210124b771baae6
Simon De Wilde <simon.dewilde2000@gmail.com>
2024-02-15 22:10:00 +0100

    add xl breakpoint too keep folderview more narrow

--- src/components/mainContent/MainContent.tsx
+++ src/components/mainContent/MainContent.tsx
@@ -5,10 +5,10 @@ import { Outlet } from 'react-router-dom';
 const MainContent: React.FC = () => {
 	return (
 		<Grid container spacing={2}>
-			<Grid xs={12} sm={5} md={4} lg={3}>
+			<Grid xs={12} sm={5} md={4} lg={3} xl={2}>
 				<FolderView />
 			</Grid>
-			<Grid xs={12} sm={7} md={8} lg={9}>
+			<Grid xs={12} sm={7} md={8} lg={9} xl={10}>
 				<Outlet />
 			</Grid>
 		</Grid>
