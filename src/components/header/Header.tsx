19fed98debaba40db4571be6c3617b5e15ecc28f
Simon De Wilde <simon.dewilde2000@gmail.com>
2024-02-15 21:51:22 +0100

    make Header responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,27 @@
-import { Box, Stack, Typography } from '@mui/joy';
+import { Grid, Typography } from '@mui/joy';
 import CookieIcon from 'assets/images/cookie-icon.svg';
 
 const Header = () => {
 	return (
-		<Box sx={{ p: 2 }}>
-			<Stack alignItems={'center'}>
-				<Stack direction={'row'} justifyContent={'center'} gap={1}>
-					<Typography level="h1">Recepten Tramstraat</Typography>
-					<Box sx={{ height: '3rem' }}>
-						<CookieIcon />
-					</Box>
-				</Stack>
+		<Grid
+			container
+			sx={{ p: 2 }}
+			justifyContent={'center'}
+			gap={2}
+			textAlign={'center'}
+		>
+			<Grid>
+				<Typography level="h1">Recepten Tramstraat</Typography>
+			</Grid>
+			<Grid sx={{ height: '3rem' }}>
+				<CookieIcon />
+			</Grid>
+			<Grid xs={12}>
 				<Typography level="body-md">
 					Recepten uit het groene schrift en meer!
 				</Typography>
-			</Stack>
-		</Box>
+			</Grid>
+		</Grid>
 	);
 };
 
